a77b80de4138f4a0ddf2fdde8954f093b7b88f8e
mo2605446cvg <mo8457ed@gmail.com>
2025-04-27 11:46:27 +0300

    Update config.php

--- config.php
+++ config.php
@@ -4,10 +4,10 @@ header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
-$host = 'alalsunacademy.com';
-$dbname = 'u343039681_alalsun';
-$username = 'u343039681_NSAR11';
-$password = 'KaReeM@K74';
+$host = '';
+$dbname = '';
+$username = '';
+$password = '';
 $conn = new mysqli($host, $username, $password, $db_name);
 
 if ($conn->connect_error) {
